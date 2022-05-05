53cc3855b01209b0d29693f4214c696d37442e2b
Chirag Mevada <chiragmevada204@gmail.com>
2022-05-05 23:53:43 +0530

    add pages

---
+++ lib/pages/cat_subcat_selection.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CatSubCatSelection extends StatefulWidget {
+  const CatSubCatSelection({Key? key}) : super(key: key);
+
+  @override
+  State<CatSubCatSelection> createState() => _CatSubCatSelectionState();
+}
+
+class _CatSubCatSelectionState extends State<CatSubCatSelection> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [],
+      ),
+    );
+  }
+}
